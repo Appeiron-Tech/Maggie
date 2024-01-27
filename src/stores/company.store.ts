3092b201ecd79bfcb5540a12a7f885fcf2a3fcda
Ernesto <ernesto@appeiron.tech>
2024-01-27 13:39:37 +0100

    Using company - contact interfaces

--- src/stores/company.store.ts
+++ src/stores/company.store.ts
@@ -1,16 +1,83 @@
-export interface ICompany {
+export interface IContact {
+  phone: string
+  email: string
+  website: string
+}
+export interface ISns {
+  code: string
+  url: string
+  active: boolean
+}
+
+export interface IStore {
   name: string
-  country: string
-  type: string
-  currency: string
+  description?: string
+  logoUrl: string
+  coverUrl: string
+  isMain: boolean
+  sns: ISns[]
+  latitude?: number
+  longitude?: number
+  cityId?: number
+  isOpenAlways: boolean
+  contact: IContact
+  isActive: boolean
   updatedAt: Date
   createdAt: Date
 }
 
-export const company = {
+export interface ICompany {
+  id: string
+  name: string
+  country: string
+  type: string
+  currency: string
+  stores: IStore[]
+  updatedAt: Date
+  createdAt: Date
+}
+
+export const company: ICompany = {
+  id: 'companyId',
   name: "Maggie Velarde",
   country: "Spain",
   type: "freelance",
   currency: "EUR",
+  stores: [
+    {
+      name: '',
+      description: '',
+      logoUrl: '',
+      coverUrl: '',
+      isMain: true,
+      isOpenAlways: true,
+      contact: {
+        phone: '+34 621 080 558',
+        email: 'maggievelar@gmail.com',
+        website: 'wwww.maggievelar.com'
+      },
+      isActive: true,
+      sns: [
+          {
+            code: 'INS',
+            url: 'www.instagram.com',
+            active: true
+          },
+          {
+            code: 'PINS',
+            url: 'www.pinterest.com',
+            active: true
+          },
+          {
+            code: 'BEH',
+            url: 'www.behance.com',
+            active: true
+          },
+      ],
+      updatedAt: new Date('2024-01-31'),
+      createdAt: new Date('2024-01-31')
+    }
+  ],
+  updatedAt: new Date('2024-01-31'),
   createdAt: new Date('2024-01-31')
 }
--- src/stores/contact.store.ts
+++ src/stores/contact.store.ts
@@ -1,45 +1,13 @@
-export interface IContact {
-  phone: string
-  email: string
-  sns: ISns[]
-}
-
-export interface ISns {
-  code: string
-  url: string
-  active: boolean
-}
-
-export interface IContactForm {
+export interface ICompanyWeb {
+  companyId: string,
   title: string,
   subtitle: string,
   sendLabel: string,
-  contact: IContact
 }
 
-export const contactForm: IContactForm = {
+export const companyWeb: ICompanyWeb = {
+  companyId: 'companyId',
   title: 'Contact',
 	subtitle: 'Do you want a special work? Let’s talk.',
-  sendLabel: 'Send',
-  contact: {
-    phone: '+34 621 080 558',
-    email: 'maggievelar@gmail.com',
-    sns: [
-      {
-        code: 'INS',
-        url: 'www.instagram.com',
-        active: true
-      },
-      {
-        code: 'PINS',
-        url: 'www.pinterest.com',
-        active: true
-      },
-      {
-        code: 'BEH',
-        url: 'www.behance.com',
-        active: true
-      },
-    ]
-  }
+  sendLabel: 'Send'
 }
