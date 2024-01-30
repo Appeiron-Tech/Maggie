22f85eb6ddcb05e4ce00ccdf84d525b455ad22ad
Ernesto <ernesto@appeiron.tech>
2024-01-30 23:35:44 +0100

    Using artwork body

--- src/stores/posts.store.ts
+++ src/stores/posts.store.ts
@@ -1,19 +1,25 @@
 export interface IWebPost {
   title: string
-  image: {
+  cover?: {
     source: string
-    link: string
+    url: string
     alt: string
   }
-  textBlocks: string[]
+  endImage?: {
+    source: string
+    url: string
+    alt: string
+  },
+  textBlocks: string[],
+  type?: string
 }
 
 export const Posts: IWebPost[] = [
   {
     title: 'Post 1',
-    image: {
+    cover: {
       source: 'https://images.squarespace-cdn.com/content/v1/554b5e7ce4b0149371f10a93/1699963908568-AXN5GNYGZW1X67RVF0CU/image-asset.jpeg?format=1000w',
-      link: 'https://www.britannica.com/list/10-famous-artworks-by-leonardo-da-vinci',
+      url: 'https://www.britannica.com/list/10-famous-artworks-by-leonardo-da-vinci',
       alt: 'image of post 1'
     },
     textBlocks: [
@@ -24,9 +30,9 @@ export const Posts: IWebPost[] = [
   },
   {
     title: 'Post 2',
-    image: {
+    cover: {
       source: 'https://images.squarespace-cdn.com/content/v1/554b5e7ce4b0149371f10a93/1694585961473-9LFYG7C05ZQAMH26H8QJ/image-asset.jpeg?format=1000w',
-      link: 'https://www.britannica.com/list/10-famous-artworks-by-leonardo-da-vinci',
+      url: 'https://www.britannica.com/list/10-famous-artworks-by-leonardo-da-vinci',
       alt: 'image of post 2'
     },
     textBlocks: [
@@ -37,9 +43,9 @@ export const Posts: IWebPost[] = [
   },
   {
     title: 'Post 3',
-    image: {
+    cover: {
       source: 'https://images.squarespace-cdn.com/content/v1/554b5e7ce4b0149371f10a93/1685970999082-ME5LDPJ8ZNCE2GEE3D6R/image-asset.jpeg?format=1000w',
-      link: 'https://www.britannica.com/list/10-famous-artworks-by-leonardo-da-vinci',
+      url: 'https://www.britannica.com/list/10-famous-artworks-by-leonardo-da-vinci',
       alt: 'image of post 3'
     },
     textBlocks: [
@@ -50,9 +56,9 @@ export const Posts: IWebPost[] = [
   },
   {
     title: 'Post 4',
-    image: {
+    cover: {
       source: 'https://images.squarespace-cdn.com/content/v1/554b5e7ce4b0149371f10a93/1672917747278-59NA4MTUUI1KXJJ9TAOI/image-asset.jpeg?format=1000w',
-      link: 'https://www.britannica.com/list/10-famous-artworks-by-leonardo-da-vinci',
+      url: 'https://www.britannica.com/list/10-famous-artworks-by-leonardo-da-vinci',
       alt: 'image of post 4'
     },
     textBlocks: [
@@ -63,9 +69,9 @@ export const Posts: IWebPost[] = [
   },
   {
     title: 'Post 5',
-    image: {
+    cover: {
       source: 'https://images.squarespace-cdn.com/content/v1/554b5e7ce4b0149371f10a93/1694585961473-9LFYG7C05ZQAMH26H8QJ/image-asset.jpeg?format=1000w',
-      link: 'https://www.britannica.com/list/10-famous-artworks-by-leonardo-da-vinci',
+      url: 'https://www.britannica.com/list/10-famous-artworks-by-leonardo-da-vinci',
       alt: 'image of post 5'
     },
     textBlocks: [
@@ -76,9 +82,9 @@ export const Posts: IWebPost[] = [
   },
   {
     title: 'Post 6',
-    image: {
+    cover: {
       source: 'https://images.squarespace-cdn.com/content/v1/554b5e7ce4b0149371f10a93/1688812103707-64F904ADD2F0JLJ4TO3G/image-asset.jpeg?format=1000w',
-      link: 'https://www.britannica.com/list/10-famous-artworks-by-leonardo-da-vinci',
+      url: 'https://www.britannica.com/list/10-famous-artworks-by-leonardo-da-vinci',
       alt: 'image of post 6'
     },
     textBlocks: [
@@ -89,9 +95,9 @@ export const Posts: IWebPost[] = [
   },
   {
     title: 'Post 7',
-    image: {
+    cover: {
       source: 'https://images.squarespace-cdn.com/content/v1/554b5e7ce4b0149371f10a93/1699963908568-AXN5GNYGZW1X67RVF0CU/image-asset.jpeg?format=1000w',
-      link: 'https://www.britannica.com/list/10-famous-artworks-by-leonardo-da-vinci',
+      url: 'https://www.britannica.com/list/10-famous-artworks-by-leonardo-da-vinci',
       alt: 'image of post 1'
     },
     textBlocks: [
@@ -102,9 +108,9 @@ export const Posts: IWebPost[] = [
   },
   {
     title: 'Post 8',
-    image: {
+    cover: {
       source: 'https://images.squarespace-cdn.com/content/v1/554b5e7ce4b0149371f10a93/1694585961473-9LFYG7C05ZQAMH26H8QJ/image-asset.jpeg?format=1000w',
-      link: 'https://www.britannica.com/list/10-famous-artworks-by-leonardo-da-vinci',
+      url: 'https://www.britannica.com/list/10-famous-artworks-by-leonardo-da-vinci',
       alt: 'image of post 2'
     },
     textBlocks: [
@@ -115,15 +121,47 @@ export const Posts: IWebPost[] = [
   },
   {
     title: 'Post 9',
-    image: {
+    cover: {
       source: 'https://images.squarespace-cdn.com/content/v1/554b5e7ce4b0149371f10a93/1685970999082-ME5LDPJ8ZNCE2GEE3D6R/image-asset.jpeg?format=1000w',
-      link: 'https://www.britannica.com/list/10-famous-artworks-by-leonardo-da-vinci',
+      url: 'https://www.britannica.com/list/10-famous-artworks-by-leonardo-da-vinci',
       alt: 'image of post 3'
     },
     textBlocks: [
       '1 Lorem Ipsum 1 es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.',
       '2 Lorem Ipsum 1 es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.',
       '3 Lorem Ipsum 1 es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.'
+    ]
+  },
+  {
+    title: 'Artwork title 1',
+    cover: {
+      source: 'https://images.squarespace-cdn.com/content/v1/554b5e7ce4b0149371f10a93/1685970999082-ME5LDPJ8ZNCE2GEE3D6R/image-asset.jpeg?format=1000w',
+      url: 'https://www.britannica.com/list/10-famous-artworks-by-leonardo-da-vinci',
+      alt: 'cover of artwork 1'
+    },
+    endImage: {
+      source: 'https://images.squarespace-cdn.com/content/v1/554b5e7ce4b0149371f10a93/1694585961473-9LFYG7C05ZQAMH26H8QJ/image-asset.jpeg?format=1000w',
+      url: 'https://www.britannica.com/list/10-famous-artworks-by-leonardo-da-vinci',
+      alt: 'end image of artwork 3'
+    },
+    textBlocks: [
+      'AW 1 Lorem Ipsum 1 es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.'
+    ]
+  },
+  {
+    title: 'Artwork title 2',
+    cover: {
+      source: 'https://images.squarespace-cdn.com/content/v1/554b5e7ce4b0149371f10a93/1685970999082-ME5LDPJ8ZNCE2GEE3D6R/image-asset.jpeg?format=1000w',
+      url: 'https://www.britannica.com/list/10-famous-artworks-by-leonardo-da-vinci',
+      alt: 'cover of artwork'
+    },
+    endImage: {
+      source: 'https://images.squarespace-cdn.com/content/v1/554b5e7ce4b0149371f10a93/1694585961473-9LFYG7C05ZQAMH26H8QJ/image-asset.jpeg?format=1000w',
+      url: 'https://www.britannica.com/list/10-famous-artworks-by-leonardo-da-vinci',
+      alt: 'end image of artwork'
+    },
+    textBlocks: [
+      'AW 2 Lorem Ipsum 1 es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.',
     ]
   },
 ]
