39a183fe8553f436fb97bcf7dd84cfe1276ee698
Ernesto <ernesto@appeiron.tech>
2024-02-06 22:11:25 +0100

    Using dynamic routes for artworks

--- src/stores/posts.store.ts
+++ src/stores/posts.store.ts
@@ -144,6 +144,7 @@ export const Posts: IWebPost[] = [
       url: 'https://www.britannica.com/list/10-famous-artworks-by-leonardo-da-vinci',
       alt: 'end image of artwork 3'
     },
+    type: 'artwork',
     textBlocks: [
       'AW 1 Lorem Ipsum 1 es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.'
     ]
@@ -160,6 +161,7 @@ export const Posts: IWebPost[] = [
       url: 'https://www.britannica.com/list/10-famous-artworks-by-leonardo-da-vinci',
       alt: 'end image of artwork'
     },
+    type: 'artwork',
     textBlocks: [
       'AW 2 Lorem Ipsum 1 es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.',
     ]
