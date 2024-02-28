c64507a1db0ae560713d4ba036f0bfa5b8b9c681
Ernesto <ernesto@appeiron.tech>
2024-02-28 22:07:53 +0100

    Using an unique sketch vue component

--- src/components/sketch/SketchBody.vue
+++ src/components/sketch/SketchBody.vue
@@ -1,15 +1,43 @@
 <script setup lang="ts">
-import { sketchStore } from '../../stores/sketch.store';
+import { ref, computed, type PropType } from 'vue';
+import type { IWebPost } from '../../stores/posts.store';
+
+const props = defineProps({
+  children: {
+    type: Array as PropType<IWebPost[]>,
+    required: true,
+  },
+})
+const sketchIndex = ref(0)
+const currentSketchChild = computed(() => {
+  return props.children.at(sketchIndex.value)
+})
+const updateIndex = (index: number) => {
+  sketchIndex.value = index
+}
 </script>
 
 <template>
-  <div class="flex flex-col gap-4">
-    <h2 class="aboutTitle pb-3">{{sketchStore.currentSketch.title}}</h2>
-    <img :src="sketchStore.currentSketch.coverImage" />
-    <div class="mainText flex justify-center">
-      <p>{{sketchStore.currentSketch.mainText}} {{sketchStore.currentSketch.mainText}}</p>
+  <div class="basis-1/6 mt-20">
+    <div class="flex flex-col gap-4">
+      <button class="dateButton" 
+        v-for="(sketchDate, index) in children" 
+        @click="updateIndex(index as any)"
+        :class="sketchIndex === index ? 'dateButtonSelected' : 'dateButton'"
+      >
+        {{sketchDate.title}}
+      </button>
     </div>
-    <img :src="sketchStore.currentSketch.endImage" />
+  </div>
+  <div class="basis-5/6 mx-24">
+    <div class="flex flex-col gap-4">
+      <h2 class="aboutTitle pb-3">{{currentSketchChild?.title}}</h2>
+      <img :src="currentSketchChild?.cover?.source" />
+      <div class="mainText flex justify-center">
+        <p>{{currentSketchChild?.textBlocks[0]}} {{currentSketchChild?.textBlocks[1]}}</p>
+      </div>
+      <img :src="currentSketchChild?.endImage?.source" />
+    </div>
   </div>
 </template>
 
@@ -33,5 +61,17 @@ h2 {
   font-size: 16px;
   font-weight: 500;
   line-height: 20px;
+}
+
+.dateButton {
+  color: #878988;
+  font-size: 20px;
+  font-weight: 500;
+  line-height: 25px;
+}
+
+.dateButtonSelected {
+  color: #363636;
+  font-weight: 700;;
 }
 </style>
--- src/stores/posts.store.ts
+++ src/stores/posts.store.ts
@@ -1,3 +1,8 @@
+export enum WebPostType {
+  POST = 'post',
+  ARTWORK = 'artwork',
+  SKETCH = 'sketch'
+}
 export interface IWebPost {
   title: string
   cover?: {
@@ -9,9 +14,12 @@ export interface IWebPost {
     source: string
     url: string
     alt: string
-  },
-  textBlocks: string[],
+  }
+  textBlocks: string[]
   type?: string
+  relatedPosts?: string[]
+  order?: number
+  children?: Omit<IWebPost, 'children'>[]
 }
 
 export const Posts: IWebPost[] = [
@@ -164,6 +172,66 @@ export const Posts: IWebPost[] = [
     type: 'artwork',
     textBlocks: [
       'AW 2 Lorem Ipsum 1 es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.',
+    ]
+  },
+  {
+    title: 'Sketch 1',
+    textBlocks: [
+      'Sketch Lorem Ipsum 1 es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.'
+    ],
+    type: WebPostType.SKETCH,
+    children: [
+      {
+        title: "Date 1",
+        cover: {
+          source: 'https://images.squarespace-cdn.com/content/v1/554b5e7ce4b0149371f10a93/1699963908568-AXN5GNYGZW1X67RVF0CU/image-asset.jpeg?format=1000w',
+          url: '',
+          alt: '' 
+        },
+        endImage: {
+          source: 'https://images.squarespace-cdn.com/content/v1/554b5e7ce4b0149371f10a93/1694585961473-9LFYG7C05ZQAMH26H8QJ/image-asset.jpeg?format=1000w',
+          url: '',
+          alt: ''
+        },
+        textBlocks: [
+          'Date 1 Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.'
+        ],
+        order: 0,
+      },
+      {
+        title: "Date 2",
+        cover: {
+          source: 'https://images.squarespace-cdn.com/content/v1/554b5e7ce4b0149371f10a93/1699963908568-AXN5GNYGZW1X67RVF0CU/image-asset.jpeg?format=1000w',
+          url: '',
+          alt: '' 
+        },
+        endImage: {
+          source: 'https://images.squarespace-cdn.com/content/v1/554b5e7ce4b0149371f10a93/1694585961473-9LFYG7C05ZQAMH26H8QJ/image-asset.jpeg?format=1000w',
+          url: '',
+          alt: ''
+        },
+        textBlocks: [
+          'Date 2 Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.'
+        ],
+        order: 1,
+      },
+      {
+        title: "Date 3",
+        cover: {
+          source: 'https://images.squarespace-cdn.com/content/v1/554b5e7ce4b0149371f10a93/1699963908568-AXN5GNYGZW1X67RVF0CU/image-asset.jpeg?format=1000w',
+          url: '',
+          alt: '' 
+        },
+        endImage: {
+          source: 'https://images.squarespace-cdn.com/content/v1/554b5e7ce4b0149371f10a93/1694585961473-9LFYG7C05ZQAMH26H8QJ/image-asset.jpeg?format=1000w',
+          url: '',
+          alt: ''
+        },
+        textBlocks: [
+          'Date 3 Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.'
+        ],
+        order: 2,
+      }
     ]
   },
 ]
