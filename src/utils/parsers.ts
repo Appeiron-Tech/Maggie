25144d83ad23fdbc2a18017219e9e2d5ca11a03a
Ernesto <ernesto@appeiron.tech>
2024-03-11 18:18:44 +0100

    Implementing own slugger

--- src/utils/parsers.ts
+++ src/utils/parsers.ts
@@ -4,3 +4,11 @@ export function removeUndefined(obj: any): any {
   Object.keys(cleanObj).forEach((key) => cleanObj[key] === undefined && delete cleanObj[key])
   return cleanObj
 }
+
+export const slugifyStr = (str: string) =>
+  str
+    .toLowerCase()
+    .trim()
+    .replace(/[^\w\s-]/g, '')
+    .replace(/[\s_-]+/g, '-')
+    .replace(/^-+|-+$/g, '')
